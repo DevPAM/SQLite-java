164f51c204a3c9ed2e09b4061a7b4e48cf70902d
Baobab <44770087+DevPAM@users.noreply.github.com>
2021-01-08 00:21:29 +0100

    Mise à jour de la classe 'DataBase'.

--- src/ca/librairies/sqlite/DataBase.java
+++ src/ca/librairies/sqlite/DataBase.java
@@ -5,13 +5,14 @@ import ca.librairies.sqlite.exceptions.SQLiteFolderNotExists;
 import ca.librairies.sqlite.exceptions.SQLiteArgumentNullException;
 
 import java.io.File;
+import java.sql.SQLException;
 
 /** The SQLite database class  */
 public abstract class DataBase extends SQLite {
     /** Initializes a new instance of {@link SQLite} class.
      * @param path_folder   Path to the database folder.
      * @param database_name Name of the database. */
-    public DataBase(String path_folder, String database_name) throws SQLiteArgumentNullException, SQLiteFolderNotExists {
+    public DataBase(String path_folder, String database_name) throws SQLiteArgumentNullException, SQLiteFolderNotExists, SQLException {
         super(path_folder, database_name);
         if(!this.databaseFileExists()) this.createTables();
     }
@@ -21,5 +22,5 @@ public abstract class DataBase extends SQLite {
         return database_file.exists();
     }
     /** Create the database tables. */
-    protected abstract void createTables();
+    protected abstract void createTables() throws SQLException, SQLiteArgumentNullException;
 }
