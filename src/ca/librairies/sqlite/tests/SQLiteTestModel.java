c9b8d38a1b65c1f1f972a656ea81378006b6423a
Baobab <44770087+DevPAM@users.noreply.github.com>
2021-01-07 17:35:48 +0100

    Ajout d'un modele de test permettant de tester le traducteur nouvellement ajouté.

---
+++ src/ca/librairies/sqlite/tests/SQLiteTestModel.java
@@ -0,0 +1,6 @@
+package ca.librairies.sqlite.tests;
+/** Model class to test the {@link ca.librairies.sqlite.SQLiteTranslator} class. */
+public class SQLiteTestModel {
+    public int id;
+    public String name;
+}
